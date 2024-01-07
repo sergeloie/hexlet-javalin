25d746b59dfab254b03ab6f6d4e60a204b4df413
anseranser <serge.loie@yandex.ru>
2024-01-07 12:10:51 +0500

    13. Валидация https://ru.hexlet.io/courses/java-web/lessons/validation/theory_unit

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,8 +2,11 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 
+import io.javalin.validation.ValidationException;
+import org.example.hexlet.dto.courses.BuildCoursePage;
 import org.example.hexlet.dto.courses.CoursePage;
 import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.dto.users.BuildUserPage;
 import org.example.hexlet.dto.users.UsersPage;
 import org.example.hexlet.model.Course;
 
@@ -21,9 +24,9 @@ public class HelloWorld {
         // Создаем приложение
         Javalin app = Javalin.create(config -> config.plugins.enableDevLogging());
 
-        Course course1 = new Course(1L,"Java", "Super Java Course");
-        Course course2 = new Course(2L,"PHP", "some php course");
-        Course course3 = new Course(3L,"JS", "Shtako JavaScript course");
+        Course course1 = new Course(1L, "Java", "Super Java Course");
+        Course course2 = new Course(2L, "PHP", "some php course");
+        Course course3 = new Course(3L, "JS", "Shtako JavaScript course");
         List<Course> courses = List.of(course1, course2, course3);
         // Описываем, что загрузится по адресу /
         app.get("/", ctx -> ctx.render("index.jte"));
@@ -32,21 +35,6 @@ public class HelloWorld {
             String name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result(String.format("Hello, %s", name));
         });
-
-//        app.get("/courses/", ctx -> {
-//
-//            String term = ctx.queryParam("term");
-//            ArrayList<Course> displayedCourses;
-//           if (term != null) {
-//               displayedCourses = courses.stream()
-//                       .filter(x -> StringUtils.containsIgnoreCase(x.getDescription(), term))
-//                       .collect(Collectors.toCollection(ArrayList::new));
-//           } else {
-//               displayedCourses = new ArrayList<>(courses);
-//           }
-//           CoursesPage page = new CoursesPage(displayedCourses, term);
-//           ctx.render("courses/index.jte", Collections.singletonMap("page", page));
-//        });
 
         app.get("/courses/", ctx -> {
             CoursesPage page = new CoursesPage(CourseRepository.getEntities(), null);
@@ -58,49 +46,73 @@ public class HelloWorld {
             ctx.render("users/index.jte", Collections.singletonMap("page", page));
         });
 
-        app.get("/courses/build", ctx -> ctx.render("courses/build.jte"));
+        app.get("/courses/build", ctx -> {
+            BuildCoursePage page = new BuildCoursePage();
+            ctx.render("courses/build.jte", Collections.singletonMap("page", page));
+
+        });
 
         app.get("/courses/{id}", ctx -> {
             Long id = ctx.pathParamAsClass("id", Long.class).get();
             Course course = courses.stream().filter(x -> x.getId().equals(id)).findFirst().get();
             CoursePage page = new CoursePage(course);
             ctx.render("courses/show.jte", Collections.singletonMap("page", page));
-//            ctx.result("Course ID: " + ctx.pathParam("id"));
         });
-//        app.get("users/{id}", ctx -> {
-//            int userID = ctx.pathParamAsClass("id", Integer.class).getOrDefault(0);
-//           ctx.result("User ID: " + userID);
-//        });
 
-        app.get("/users/build", ctx -> ctx.render("users/build.jte"));
+
+        app.get("/users/build", ctx -> {
+            BuildUserPage page = new BuildUserPage();
+            ctx.render("users/build.jte", Collections.singletonMap("page", page));
+        });
 
 
 
         app.get("/users/{id}", ctx -> {
-           String id = ctx.pathParam("id");
-           String escapedId = StringEscapeUtils.escapeHtml4(id);
-           ctx.contentType("text/html");
-           ctx.result(escapedId);
+            String id = ctx.pathParam("id");
+            String escapedId = StringEscapeUtils.escapeHtml4(id);
+            ctx.contentType("text/html");
+            ctx.result(escapedId);
         });
 
         app.post("/users", ctx -> {
             String name = ctx.formParam("name");
             String email = ctx.formParam("email").trim().toLowerCase();
-            String password = ctx.formParam("password");
-            String passwordConfirmation = ctx.formParam("passwordConfirmation");
 
-            User user = new User(name, email, password);
-            UserRepository.save(user);
-            ctx.redirect("/users");
+            try {
+                String passwordConfirmation = ctx.formParam("passwordConfirmation");
+                String password = ctx.formParamAsClass("password", String.class)
+                        .check(value -> value.equals(passwordConfirmation), "Passwords not match")
+                        .get();
+                User user = new User(name, email, password);
+                UserRepository.save(user);
+                ctx.redirect("/users");
+            } catch (ValidationException e) {
+                BuildUserPage page = new BuildUserPage(name, email, e.getErrors());
+                ctx.render("users/build.jte", Collections.singletonMap("page", page));
+            }
+
+
+
         });
 
         app.post("/courses", ctx -> {
-            String name = ctx.formParam("name");
-            String description = ctx.formParam("description");
+            String name = null;
+            String description = null;
 
-            Course course = new Course(CourseRepository.getEntities().size()+1L, name, description);
-            CourseRepository.save(course);
-            ctx.redirect("/courses");
+            try {
+                name = ctx.formParamAsClass("name", String.class)
+                        .check(value -> value.length() > 2, "Course name must be more than 2 characters")
+                        .get();
+                description = ctx.formParamAsClass("description", String.class)
+                        .check(value -> value.length() > 10, "Course description must be more than 10 characters")
+                        .get();
+                Course course = new Course(CourseRepository.getEntities().size() + 1L, name, description);
+                CourseRepository.save(course);
+                ctx.redirect("/courses");
+            } catch (ValidationException e) {
+                BuildCoursePage page = new BuildCoursePage(name, description, e.getErrors());
+                ctx.render("courses/build.jte", Collections.singletonMap("page", page));
+            }
         });
 
 
